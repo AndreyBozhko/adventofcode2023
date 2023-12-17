0e74d5c31c0eac2e273998e9ee7b7be3e6d02126
Andrey Bozhko <andybozhko@gmail.com>
2023-12-16 19:35:38 -0600

    update day 14

--- src/main/kotlin/aoc/day14.kt
+++ src/main/kotlin/aoc/day14.kt
@@ -2,38 +2,71 @@ package aoc
 
 private val input = readLines("14.txt")
 
-fun calculateLoad(platform: List<String>, column: Int): Int {
-    var i1 = platform.indexOfFirst { it[column] != '#' }
-    if (i1 == -1) {
-        return 0
-    }
+private typealias Platform = Array<CharArray>
 
-    var total = 0
-    var rocks = 0
-    for (i2 in i1 .. platform.size) {
-        if (i2 == platform.size || platform[i2][column] == '#') {
-            val range = (platform.size - i1 - rocks + 1 .. platform.size - i1)
-            total += range.sum()
-            i1 = i2 + 1
-            rocks = 0
-        } else if (platform[i2][column] == 'O') {
-            rocks += 1
+fun Platform.rotateClockwise(): Platform = Array(this[0].size) { x ->
+    CharArray(this.size) { y -> this[this[0].size - 1 - y][x] }
+}
+
+fun Platform.slide() = forEach { arr ->
+    var left = arr.indexOfFirst { it != '#' }
+    if (left == -1) return@forEach
+
+    for (right in left .. arr.size) {
+        if (right == arr.size || arr[right] == '#') {
+            arr.sort(fromIndex = left, toIndex = right)
+            left = right + 1
         }
     }
-    return total
+}
+
+fun Platform.state(): String = joinToString("") { it.concatToString() }
+
+fun CharArray.calculateLoad(): Int = foldIndexed(0) { idx, acc, c ->
+    acc + if (c == 'O') idx+1 else 0
 }
 
 fun main() {
+    val platform: Platform = input.map { it.toCharArray() }.toTypedArray()
+
     // part A
     run {
+        val p = platform.rotateClockwise().apply { slide() }
 
+        val result = p.sumOf { it.calculateLoad() }
+        println(result)
     }
 
     // part B
     run {
-        val result = input[0].indices.sumOf {
-            calculateLoad(input, column = it)
+        val numCycles = 1000000000
+
+        var tmp = platform
+        val seenMap = mutableMapOf<String, Int>()
+        seenMap[platform.state()] = 0
+
+        val seenList = mutableListOf(platform)
+
+        for (i in 1..numCycles) {
+            tmp = tmp.rotateClockwise().apply { slide() }
+            tmp = tmp.rotateClockwise().apply { slide() }
+            tmp = tmp.rotateClockwise().apply { slide() }
+            tmp = tmp.rotateClockwise().apply { slide() }
+
+            val state = tmp.state()
+            if (state in seenMap) {
+                break
+            }
+            seenMap[state] = i
+            seenList += tmp
         }
+
+        val idx2 = seenMap.size
+        val idx1 = seenMap[tmp.state()]!!
+        val idx = (numCycles - idx1) % (idx2 - idx1) + idx1
+
+        val p = seenList[idx].rotateClockwise()
+        val result = p.sumOf { it.calculateLoad() }
         println(result)
     }
 }
